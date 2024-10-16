9111aa1488c24a171ec2c4819bf84b63ece5d06e
Zolane <110057971+Zolane@users.noreply.github.com>
2024-10-16 20:51:47 +0200

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+/**
+ *
+ * @author ttjel
+ */
+public class BasicFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //declare the variables
+        MyFirstFrame myFrame;
+        
+        //instantiate the frame
+        myFrame=new MyFirstFrame();
+        
+        
+        
+        
+    }
+    
+}
---
+++ MyFirstFrame.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package za.ac.tut.ui;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ttjel
+ */
+public class MyFirstFrame extends JFrame {
+    
+    public MyFirstFrame()
+    {
+        setTitle("My first GUI");
+        setSize(450,300);
+        setDefaultLookAndFeelDecorated(true);
+        setResizable(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setForeground(Color.BLUE);
+        setVisible(true);
+    }
+    
+}
